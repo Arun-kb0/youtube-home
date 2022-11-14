6b4d8740db5e3875c6c20bb4a5c15eaa8010dc97
noreplay <noreplay>
2022-11-14 19:42:03 +0530

    navbar added using js and css

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 //video play on hover 
 $(document).ready(function () {
+  var url=""
   $('.video').each(function (el) {
     var _this = $(this);
+    var  url = ""
     _this.on('mouseover', function (ev) {
-      _this[0].src += "?&autoplay=1&mute=1";
-      ev.preventDefault();
-      //console.log(_this[0])
+       if(url.length >0){
+        tmp  = url.replace("?&autoplay=0","?&autoplay=1&mute=1")
+        _this[0].src= url
+       }else{
+        _this[0].src += "?&autoplay=1&mute=1";
+        url = _this[0].src
+       }
+     
+      console.log("mouse in")
+      console.log(_this[0])
 
     });
     _this.on('mouseout', function (ev) {
-      _this[0].src += "?autoplay=0"
       ev.preventDefault()
+      //_this[0].src += "?&autoplay=0"
+      //_this[0].src = url+"?&autoplay=0"
+       var tmp  = url.replace("?&autoplay=1&mute=1","?&autoplay=0")
+      _this[0].src=tmp
+      console.log("mouse out")
+  
+      console.log(_this[0])
+
+
+      
     })
   });
 });
@@ -38,8 +56,13 @@ $(document).ready(function () {
 $(document).ready(function () {
   const menuToggle = document.querySelectorAll('.ham-btn')
   const navigation = document.querySelectorAll('.navigation')
-  //console.log(navigation[0])
-  menuToggle[0].onclick= function () {
+  const showMore = document.querySelectorAll(".showmore")
+  const showLess= document.querySelectorAll('shrink')
+  const secEnd = document.querySelectorAll("#showmore-secEnd")
+  
+ 
+  menuToggle[0].onclick= function (e) {
+    e.preventDefault()
     
     menuToggle[0].classList.toggle("active");
     navigation[0].classList.toggle('active')
@@ -47,5 +70,23 @@ $(document).ready(function () {
     console.log(navigation)
     console.log(menuToggle)
   }
+
+  
+  showMore[0].onclick= (e)=>{
+    e.preventDefault()
+    showMore[0].classList.toggle("active")
+    secEnd[0].classList.toggle("active")
+    console.log(secEnd)
+    
+  }
+
+  showLess[0].onclick=(e)=>{
+    e.preventDefault()
+    showMore[0].classList.toggle("")
+    console.log(showLess)
+
+  }
+
+  
 })
 
